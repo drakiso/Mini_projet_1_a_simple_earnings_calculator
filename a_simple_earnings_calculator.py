b7a4d0df6513ef05c27a2b7e19c4cdc18c935a4f
Dr AKIso <anzankisrael@yahoo.com>
2023-06-04 01:17:33 +0300

    add new modifications

--- a_simple_earnings_calculator.py
+++ a_simple_earnings_calculator.py
@@ -1,10 +1,10 @@
 """A simple console application that help an employer
 calculate an employee's earning in a given week."""
 
-name: str = input("Please enter the employee's name:    ").strip().title()
-wage: float = float(input("Please enter the employee's hourly wage:    "))
-hours: float = float(input("How many hours the employee worked:    "))
+employee_s_name: str = input("Please enter the employee's name:    ").strip().title()
+hourly_wage: float = float(input("Please enter the employee's hourly wage:    "))
+worked_hours: float = float(input("How many hours the employee worked:    "))
 
-earnings: float = wage * hours
+employee_s_earnings: float = hourly_wage * worked_hours
 
-print(f"{name} earned ${earnings:.2f} this week")
+print(f"{employee_s_name} earned ${employee_s_earnings:.2f} a week")
